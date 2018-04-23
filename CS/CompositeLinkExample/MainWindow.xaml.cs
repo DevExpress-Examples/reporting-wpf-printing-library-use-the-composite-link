4ef72389311998d6209ac223054a5486aa904f15
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CompositeLinkExample/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Printing;
+// ...
+
+namespace CompositeLinkExample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void printButton_Click(object sender, RoutedEventArgs e) {
+            List<TemplatedLink> links = new List<TemplatedLink>();
+            links.Add(new PrintableControlLink((TableView)categoriesGrid.View));
+            links.Add(new PrintableControlLink((TableView)productsGrid.View));
+
+            CompositeLink compositeLink = new CompositeLink(links);
+            PrintHelper.ShowRibbonPrintPreview(this, compositeLink);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use the Composite Link
+
+
+This example demonstrates how to use the <strong>CompositeLink</strong> class to combine contents of several printing links within a single document.
+
+<br/>
+
+
