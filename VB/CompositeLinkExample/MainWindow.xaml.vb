8a055405bd7ed79f3069b67594ff274c28a96626
DevExpressExampleBot <fakeEmail>
2024-01-30 18:39:35 +0400

    Source auto update [skip ci]

--- VB/CompositeLinkExample/MainWindow.xaml.vb
+++ VB/CompositeLinkExample/MainWindow.xaml.vb
@@ -1,27 +1,23 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Documents
 Imports DevExpress.Xpf.Grid
 Imports DevExpress.Xpf.Printing
-' ...
 
+' ...
 Namespace CompositeLinkExample
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub printButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim links As New List(Of TemplatedLink)()
-            links.Add(New PrintableControlLink(CType(categoriesGrid.View, TableView)))
-            links.Add(New PrintableControlLink(CType(productsGrid.View, TableView)))
-
-            Dim compositeLink As New CompositeLink(links)
+            Dim links As List(Of TemplatedLink) = New List(Of TemplatedLink)()
+            links.Add(New PrintableControlLink(CType(Me.categoriesGrid.View, TableView)))
+            links.Add(New PrintableControlLink(CType(Me.productsGrid.View, TableView)))
+            Dim compositeLink As CompositeLink = New CompositeLink(links)
             PrintHelper.ShowRibbonPrintPreview(Me, compositeLink)
         End Sub
     End Class
